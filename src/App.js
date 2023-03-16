7046bf6535b893af98a72ad0fddd74205f5b85e5
jade-arinal-banares <jade.arinal.banares@gmail.com>
2023-03-16 11:56:30 +0800

    added: react js

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import About from "./assets/modules/About";
+import Contact from "./assets/modules/Contact";
+
+export default function App() {
+  return (
+    <>
+      <div className="App">
+        <About />
+        <Contact />
+      </div>
+    </>
+  );
+}
---
+++ src/assets/modules/About.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+
+export default class About extends Component {
+  render() {
+    return (
+      <>
+        {/* Art Galler Section */}
+        <section className="hide-for-mobile tech-used art-gallery">
+          <h2 className="reveal fade-bottom cursive">Art Gallery</h2>
+          <p className="reveal fade-bottom">
+            I do some sketching and portrait in my free time. Feel free to visit
+            this page
+          </p>
+          <button className="reveal fade-bottom" id="artrelated">
+            View Gallery
+          </button>
+        </section>
+      </>
+    );
+  }
+}
