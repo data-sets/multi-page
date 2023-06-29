d4484d44b1a89b0d453e4ea94c48f5c850a935ee
jade-arinal-banares <jade.arinal.banares@gmail.com>
2023-06-29 12:56:03 +0800

    added: contact page section

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Index from "../src/assets/modules/Index";
 import NoMatch from "../src/assets/modules/NotFound";
 import About from "../src/assets/modules/pages/About/About";
-import Contact from "../src/assets/modules/pages/Contact";
+import Contact from "../src/assets/modules/pages/Contact/Contact";
 import "../src/dist/style.css";
 
 export default function App() {
---
+++ src/assets/modules/pages/Contact/Contact.jsx
@@ -0,0 +1,82 @@
+import { Helmet } from "react-helmet";
+import Navigation from "../../Navigation";
+import Footer from "../../Footer";
+import Chart from "../../../images/icon-chart.svg";
+import Cog from "../../../images/icon-cog.svg";
+import User from "../../../images/icon-person.svg";
+import ShapeLeft from "../../../images/bg-pattern-about-2-contact-1.svg";
+import ShapeRight from "../../../images/bg-pattern-contact-2.png";
+import ShapeSmall from "../../../images/bg-pattern-contact-2-1.png";
+
+export default function Contact() {
+  return (
+    <>
+      <Helmet>
+        <title>Contact Page</title>
+      </Helmet>
+      <header>
+        <Navigation />
+      </header>
+      <section className="contact-section">
+        <div className="contact-section__shape">
+          <img src={ShapeLeft} alt="" />
+        </div>
+        <div className="contact-section__description">
+          <div>
+            <h1>Contact</h1>
+            <h2>Ask us about</h2>
+            <ul>
+              <li>
+                <img src={User} alt="User" />
+                <p>The quality of our talent network</p>
+              </li>
+              <li>
+                <img src={Cog} alt="Cog" />
+                <p>Usage & implementation of our software</p>
+              </li>
+              <li>
+                <img src={Chart} alt="Chart" />
+                <p>How we help drive innovation</p>
+              </li>
+            </ul>
+          </div>
+          <div>
+            <form action="/" method="POST">
+              <div>
+                <input type="text" name="name" placeholder="Name" />
+              </div>
+              <div>
+                <input type="text" name="name" placeholder="Email Address" />
+              </div>
+              <div>
+                <input type="text" name="name" placeholder="Company Name" />
+              </div>
+              <div>
+                <input type="text" name="name" placeholder="Title" />
+              </div>
+              <textarea
+                name=""
+                id=""
+                cols="20"
+                rows="10"
+                placeholder="Message"
+              ></textarea>
+              <div>
+                <button>submit</button>
+              </div>
+            </form>
+          </div>
+        </div>
+        <div className="contact-section__right">
+          <img src={ShapeRight} alt="" />
+        </div>
+        <div className="contact-section__small">
+          <img src={ShapeSmall} alt="" />
+        </div>
+      </section>
+      <div className="footer-sec">
+        <Footer />
+      </div>
+    </>
+  );
+}
